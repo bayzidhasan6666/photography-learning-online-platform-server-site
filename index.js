ca2f56c1abe553cf392f1f3337bbe92c758ccef6
bayzidhasan6666 <121633867+bayzidhasan6666@users.noreply.github.com>
2023-06-07 19:48:44 +0800

    server run

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Visual Learning Is running....');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
