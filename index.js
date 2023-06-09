04fb667e86e73618d84cec11bb3ccce69b93a5bb
bayzidhasan6666 <121633867+bayzidhasan6666@users.noreply.github.com>
2023-06-09 08:19:21 +0800

    get user from database

--- index.js
+++ index.js
@@ -74,10 +74,22 @@ async function run() {
     //   next();
     // };
 
-    // users related api
+    // ---------------users related api
+
+    // get users from database
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
 
     app.post('/users', async (req, res) => {
       const user = req.body;
+      console.log(user);
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists' });
+      }
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
